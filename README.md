15f6061e6af57e427789b84520a017b3d4efdf43
hrpatil273 <mail.harshwardhanpatil@gmail.com>
2024-04-07 11:04:23 -0400

    Added requirements and setup

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # kidney-disease-classification
 This is an End to end project implementation for classifying kidney diseases.
+
+# How to run
+## Steps:
+
+### STEP 01 - Clone the repository 
+```bash
+git clone https://github.com/hrpatil273/kidney-disease-classification.git
+```
+
+### STEP 02 - Create a conda environment after opening the repository
+
+```bash
+conda create -n kidney python=3.11 -y
+```
+
+```bash
+conda activate kidney 
+```
+
+
+### STEP 03 - Install the requirements
+
+```bash 
+pip install -r requirements.txt
+```
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "kidney-disease-classification"
+AUTHOR_USER_NAME = "hrpatil273"
+SRC_REPO = "classifier"
+AUTHOR_EMAIL = "mail.harshwardhanpatil@gmail.com"
+
+setuptools.setup(
+    name = SRC_REPO,
+    version = __version__,
+    author = AUTHOR_USER_NAME,
+    author_email = AUTHOR_EMAIL,
+    description = "A Python package for classifying Kidney Diseases",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src")
+
+)
